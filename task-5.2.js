a6e91a21d2cdce5cc56ba1c35acc8d9929cc99f0
Ashraful Islam <ashraful171790@gmail.com>
2026-07-24 20:33:14 +0600

    task-5.2 is done

---
+++ task-5.2.js
@@ -0,0 +1,12 @@
+/**
+ * todo:                    topic:
+ * 
+ * Task 5.2 — Watch the Video Concept:
+ * 
+    After watching the "What is an Object" video, write 2–3 sentences (as a comment in your code) explaining an object in your own words, using a real-life analogy (not a car or a person — get creative!).
+
+ */
+
+    /**
+     *  object হলো এমন এক ধরনের data type. যেখানে key and value pair এর সংগ্রহ । যেখানে বিভিন্ন তথ্য গুছিয়ে রাখে।  যেমনঃ smart phone: name: 'i phone' , brand; 'apple', color: 'orange' 
+     */
